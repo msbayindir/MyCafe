6e0379a9922c889af9c842dee4ab2b974292710a
SEFA1905 <70607438+SEFA1905@users.noreply.github.com>
2022-03-20 01:03:54 +0300

    first commit

---
+++ MyCafe.nTier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCafe.nTier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MyCafeMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyCafeMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
