8e1832681de51756c577e33579e2535cb7406b20
SEFA1905 <70607438+SEFA1905@users.noreply.github.com>
2022-04-08 03:24:43 +0300

    Entities Created

---
+++ MyCafeDAL/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MyCafeDAL.Entities
+{
+	public class Product
+	{
+        public int Id { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public byte[]? Image { get; set; }
+
+
+       
+
+    }
+}
+
